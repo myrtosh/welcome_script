7e55f51bc6aca9b01d1ba5f3780119c0446b84f5
myrtosh <myrto-sherin@hotmail.com>
2022-09-30 16:46:26 +0200

    created script to welcome you everytime new terminal opens

---
+++ welcome.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/zsh
+echo "Welcome $LOGNAME"
+echo -en '\n'
+echo -en '\n'
+dateAndTime=$(date)
+echo "Current date and time is: $dateAndTime"
+echo -en '\n'
+echo -en '\n'
+curl wttr.in/
+echo -en '\n'
+echo -en '\n'
+echo "'You are the greatest project you will ever work on'"
